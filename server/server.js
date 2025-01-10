9aba6bd4413c7b43803d4cb15867bbbc88ec9db7
Ricards Rudups <ricardsrudups99@gmail.com>
2025-01-10 14:27:08 +0000

    set up server, connecting to db.

--- server/server.js
+++ server/server.js
@@ -9,3 +9,19 @@ app.use(express.json());
 app.listen(8080, function() {
      console.log(`Server running on PORT 8080`);
 });
+
+
+app.get("/", (req, res) => {
+     res.json({ message: "This is the root route. It's ALIVVEE!" });
+   });
+
+
+   import pg from "pg";
+   import dotenv from "dotenv";
+   dotenv.config();
+
+const dbConnectionString = process.env.DB_CONN_STRING;
+
+   const db = new pg.Pool({
+     connectionString: dbConnectionString
+   })
